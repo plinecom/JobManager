6275b9d1e9a43a152f537ff6efae422b3d2c9ab2
plinecom <plinecom@gmail.com>
2015-12-14 17:06:29 +0900

    GUI test 2

--- gui/submit/common.py
+++ gui/submit/common.py
@@ -8,10 +8,47 @@ class CommonPanel(QtGui.QWidget):
         self.init_ui()
 
     def init_ui(self):
+        height = 0
+        layout = QtGui.QGridLayout()
         self.jobname_label = QtGui.QLabel("job name")
         self.jobname_qle = QtGui.QLineEdit(self)
-        layout = QtGui.QGridLayout()
-        layout.addWidget(self.jobname_label, 0, 0)
-        layout.addWidget(self.jobname_qle, 0, 1)
+
+        layout.addWidget(self.jobname_label, height, 0)
+        layout.addWidget(self.jobname_qle, height, 1)
+
+        height += 1
+
+        self.pj_path_label = QtGui.QLabel("proj path")
+        self.pj_path_qle = QtGui.QLineEdit(self)
+        layout.addWidget(self.pj_path_label, height, 0)
+        layout.addWidget(self.pj_path_qle, height, 1)
+
+        height += 1
+
+        self.scene_path_label = QtGui.QLabel("scene path")
+        self.scene_path_qle = QtGui.QLineEdit(self)
+        layout.addWidget(self.scene_path_label, height, 0)
+        layout.addWidget(self.scene_path_qle, height, 1)
+
+        height += 1
+
+        self.manager_label = QtGui.QLabel("manager")
+        self.manager_qle = QtGui.QLineEdit(self)
+        layout.addWidget(self.manager_label, height, 0)
+        layout.addWidget(self.manager_qle, height, 1)
+
+        height += 1
+
+        self.app_label = QtGui.QLabel("application")
+        self.app_qle = QtGui.QLineEdit(self)
+        layout.addWidget(self.app_label, height, 0)
+        layout.addWidget(self.app_qle, height, 1)
+
+        height += 1
+
+        self.renderer_label = QtGui.QLabel("renderer")
+        self.renderer_qle = QtGui.QLineEdit(self)
+        layout.addWidget(self.renderer_label, height, 0)
+        layout.addWidget(self.renderer_qle, height, 1)
 
         self.setLayout(layout)
