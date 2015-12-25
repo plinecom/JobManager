fdc164cf6f43a5e9bc82dcd8d20b8796a5c85b0f
plinecom <plinecom@gmail.com>
2015-12-25 21:30:21 +0900

    fix config

--- config/config.py
+++ config/config.py
@@ -3,6 +3,7 @@ class ConfigInfo:
         self._configlist = configList
 
     def getvalue(self,key):
+        dic={}
         for config_doc in self._configlist:
 #          print config_doc
             for config_title in config_doc.keys():
@@ -11,8 +12,13 @@ class ConfigInfo:
                     #print value
                     if isinstance(value, unicode):
                         return value.encode('utf-8')
+                    if isinstance(value,dict):
+                        dic.update(value)
                     else:
                         return value
-        return None
+        if len(dic.keys()) > 0:
+            return dic
+        else:
+            return None
     def getparam(self):
         return self._configlist
