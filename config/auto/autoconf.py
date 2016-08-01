71edb3d1671d8da937b3206de765c32af2d79a50
Masataka Koike <plinecom@gmail.com>
2016-08-01 13:46:13 +0900

    Refactor

--- config/auto/autoconf.py
+++ config/auto/autoconf.py
@@ -3,12 +3,13 @@ import platform
 import json
 import os.path
 
+
 class AutoConfig:
     def __init__(self):
 
-        self._param = {"auto_config":{}}
+        self._param = {"auto_config": {}}
 
-        system = platform.system() # Windows, Linux, Darwin
+        system = platform.system()  # Windows, Linux, Darwin
         if "Darwin" in system:
             system = "MacOS_X"
         self._param["auto_config"]["system"] = system
@@ -20,13 +21,13 @@ class AutoConfig:
         else:
             pathList = os.environ['PATH'].split(':')
 
-        print pathList
+        # print pathList
         script_dir_path = os.path.abspath(os.path.dirname(__file__))
         config_path = os.path.join(script_dir_path,"autoconf.json")
         config_file = open(config_path,'r')
         config = json.load(config_file)
 
-        #search dispatcher
+        # search dispatcher
 
         self._param["auto_config"]["dispatcher"] = {}
         for dispatcher_name in config["dispatcher"].keys():
@@ -35,23 +36,21 @@ class AutoConfig:
                 continue
 #            print "test"
 #            print  config[dispatcher_name]["submitter"][system].replace('\\\\','\\')
-            submit_path =  config["dispatcher"][dispatcher_name]["submitter"][system].replace('\\\\','\\')
+            submit_path =  config["dispatcher"][dispatcher_name]["submitter"][system].replace('\\\\', '\\')
             if os.path.exists(submit_path):
                 self._param["auto_config"]["dispatcher"][dispatcher_name] = {}
                 for param_key in config["dispatcher"][dispatcher_name].keys():
-                    self._param["auto_config"]["dispatcher"][dispatcher_name][param_key] = config["dispatcher"][dispatcher_name][param_key][system].replace('\\\\','\\')
+                    self._param["auto_config"]["dispatcher"][dispatcher_name][param_key] = config["dispatcher"][dispatcher_name][param_key][system].replace('\\\\', '\\')
 
+                # print "path"
 
-                print "path"
-
-        print self._param["auto_config"]["dispatcher"]
+        # print self._param["auto_config"]["dispatcher"]
         # scan dispatcher typical point
         # scan dispatcher $PATH
 
-        #scan registry  x too many times, too slow
+        # scan registry  x too many times, too slow
 
         # search maya_executable
 
     def getparam(self):
         return self._param
-
