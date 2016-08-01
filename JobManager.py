c6593d20af2e5f133970e3c42a775dcf4f9824c9
Masataka Koike <plinecom@gmail.com>
2016-08-01 13:29:28 +0900

    Refactor

--- JobManager.py
+++ JobManager.py
@@ -17,10 +17,10 @@ import job.jobinfo
 
 
 class MainWindow(QtGui.QMainWindow):
-    def __init__(self,jobList, dispatcherList,configInfo, parent=None):
+    def __init__(self, jobList, dispatcherList, configInfo, parent=None):
         QtGui.QMainWindow.__init__(self,parent)
         self._joblist = jobList
-        print self._joblist.get_joblist()
+        # print self._joblist.get_joblist()
         self._dipatcherList = dispatcherList
         self._configInfo = configInfo
 
@@ -28,19 +28,18 @@ class MainWindow(QtGui.QMainWindow):
         panel = QtGui.QWidget()
         panel.setObjectName("commonPanel")
 
-        #main_window.setAcceptDrops(True)
+        # main_window.setAcceptDrops(True)
 
-        fileinfo_tab = gui.submit.fileinfo.fileinfo.FileinfoPanel(jobList, dispatcherList,configInfo, panel)
+        file_info_tab = gui.submit.fileinfo.fileinfo.FileinfoPanel(jobList, dispatcherList, configInfo, panel)
 
-        dispatcher_common_panel = gui.submit.dispatcher.common.CommonPanel(jobList, dispatcherList,configInfo, panel)
+        dispatcher_common_panel = gui.submit.dispatcher.common.CommonPanel(jobList, dispatcherList, configInfo, panel)
         qube_panel = gui.submit.dispatcher.qube.QubePanel(jobList, panel)
         qtabRLow = QtGui.QTabWidget()
         qtabRLow.addTab(dispatcher_common_panel, "dispatcher")
         qtabRLow.addTab(qube_panel, "Qube")
 
-
         panel_layout = QtGui.QVBoxLayout()
-        panel_layout.addWidget(fileinfo_tab)
+        panel_layout.addWidget(file_info_tab)
         panel_layout.addWidget(qtabRLow)
 
         button_submit =QtGui.QPushButton("sumbmit")
@@ -54,16 +53,12 @@ class MainWindow(QtGui.QMainWindow):
 
         #main_window.setAcceptDrops(True)
 
-
-        qsplitV =QtGui.QSplitter(QtCore.Qt.Vertical)
+        qsplitV = QtGui.QSplitter(QtCore.Qt.Vertical)
         panelL_layout = QtGui.QVBoxLayout()
         qtabL = QtGui.QTabWidget()
-        self._list_view = gui.joblist.list.JobListView(jobList, dispatcherList,configInfo, panel)
+        self._list_view = gui.joblist.list.JobListView(jobList, dispatcherList, configInfo, panel)
 
         qtabL.addTab( self._list_view, "Job")
-
-
-
 
         txtBrowser = QtGui.QTextBrowser()
         txtBrowser.append("test")
@@ -79,12 +74,8 @@ class MainWindow(QtGui.QMainWindow):
 
         qsplitH = QtGui.QSplitter(QtCore.Qt.Horizontal)
 
-
-
         qsplitH.addWidget(panelL)
         qsplitH.addWidget(panel)
-
-
 
         self.setWindowTitle("JobManager")
         self.setCentralWidget(qsplitH)
@@ -97,9 +88,9 @@ class MainWindow(QtGui.QMainWindow):
 
     def dropEvent(self, event):
         for u in event.mimeData().urls():
-            print u.toLocalFile()
+            # print u.toLocalFile()
             file_parser = filelib.parser.lib.fileParse(str(u.toLocalFile()))
-            print file_parser.getparam()
+            # print file_parser.getparam()
             self._joblist.get_joblist().append(
                     job.jobinfo.JobInfo(
                             file_parser.getparam(),
@@ -108,14 +99,14 @@ class MainWindow(QtGui.QMainWindow):
                     )
             )
 
-            print self._joblist.get_current_job()
+            # print self._joblist.get_current_job()
 
         self._joblist.set_current_job_id(-1)
         self.update_ui()
 
     def update_ui(self):
-#        common_panel = self.findChild(gui.submit.fileinfo.common.CommonPanel)
-#        common_panel.update_ui()
+        # common_panel = self.findChild(gui.submit.fileinfo.common.CommonPanel)
+        # common_panel.update_ui()
         self.update_ui_right_pannel()
         self._list_view.update_ui()
 
@@ -123,12 +114,10 @@ class MainWindow(QtGui.QMainWindow):
         tab_panel = self.findChild(gui.submit.fileinfo.fileinfo.FileinfoPanel)
         tab_panel.update_ui()
 
-
     def on_button_submit(self):
         dispatcher = self._joblist.get_current_job().getValue("dispatherObj")
         dispatcher.submit(self._joblist.get_current_job())
         print "submit"
-
 
 
 def loadFile(absPath):
@@ -137,54 +126,52 @@ def loadFile(absPath):
 
 if __name__ == "__main__":
 
-
-    #addFilePath = "/root/test_maya_2015.mb"
+    # addFilePath = "/root/test_maya_2015.mb"
     addFilePath = None
 
-    if(len(sys.argv) > 1):
+    if len(sys.argv) > 1:
         addFilePath = sys.argv[1]
-
 
     auto_config = config.auto.autoconf.AutoConfig().getparam()
 
-
     script_dir_path = os.path.abspath(os.path.dirname(__file__))
     config_path = os.path.join(script_dir_path,"config","config.yml")
-    print config_path
+    # print config_path
     config_file = open(config_path,'r')
     config_data = yaml.load(config_file)
 
     config_data["00"] = auto_config
-    print config
+    # print config
     configList=[]
-    #configList.append(auto_config["dispatcher"])
+    # configList.append(auto_config["dispatcher"])
     for key in sorted(config_data.keys(), reverse=True):
         configList.append(config_data[key])
 
-    print configList
+    # print configList
     print json.dumps(configList, sort_keys=False, indent=4)
     configInfo = config.config.ConfigInfo(configList)
 #    print configInfo.getvalue("priority")
 #    print configInfo.getvalue("priority")
     configInfo.getvalueJsonPath("[*].*.priority")
+    print "extrt"
     print configInfo.getvalueJsonPath("[*].*.dispatcher")
-    #command line mode?
+    # command line mode?
 
     # loadFile(addFilePath)
     fileParam = {}
     if addFilePath is not None:
         fileParser = filelib.parser.lib.fileParse(addFilePath)
-        print fileParser.getparam()
+        # print fileParser.getparam()
         fileParam = fileParser.getparam()
 #
 #    job_factory = jobfactory.factory.JobFactory()
 #    job = job_factory.getJob(fileParser.getparam(), "SudioPlugin()")
     dispatcherList = []
     for dispatcher_info in  configInfo.getvalueJsonPath("[*].*.dispatcher"):
-        print dispatcher_info
+        # print dispatcher_info
         if u"Qube6" in dispatcher_info.keys():
             dispatcherList.append(dispatcher.qube.Qube6(configInfo.getvalueJsonPath("[*].*.dispatcher.Qube6")[0]).getparam())
-    print dispatcherList
+    # print dispatcherList
 
     jobList = job.jobinfo.JobInfoList()
     if addFilePath is not None:
@@ -195,7 +182,6 @@ if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     main_window = MainWindow(jobList,dispatcherList,configInfo.getvalueJsonPath("[*]"))
     main_window.show()
-
 
     sys.exit(app.exec_())
 #    dispatcher.submit(job)
