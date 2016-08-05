faca97f6905a52457e94fc8fcadf79144ae4bf93
Masataka Koike <plinecom@gmail.com>
2016-08-05 16:52:58 +0900

    Refactor

--- filelib/parser/mb.py
+++ filelib/parser/mb.py
@@ -1,86 +1,88 @@
-__author__ = 'Masataka'
 import struct
 import binascii
 import filelib.parser.ma
 
+__author__ = 'Masataka'
+
 
 class FileParserMayaMB(filelib.parser.ma.FileParserMaya):
-    def __init__(self, filepath):
-       filelib.parser.ma.FileParserMaya.__init__(self, filepath)
+    def __init__(self, file_path):
+        filelib.parser.ma.FileParserMaya.__init__(self, file_path)
 
+    @property
     def parse_process(self):
 
         version = None
         renderer = None
 
-        frameStart = None
-        frameEnd = None
-        byFrameSteps = None
-        fpsData = None
-        typeInfo = None
+        frame_start = None
+        frame_end = None
+        by_frame_steps = None
+        fps_data = None
+        type_info = None
 
-        outputFileExt = None
-        outputFileExtVray = None
-        outputPathFormat = None
-        outputPathFormatVray = None
+        output_file_ext = None
+        output_file_ext_vray = None
+        output_path_format = None
+        output_path_format_vray = None
 
-        renderLayerList = []
-        renderlayerEnable = {}
-        cameraList = ['default']
+        render_layer_list = []
+        render_layer_enable = {}
+        camera_list = ['default']
 
-#        for renderLayer in job["renderLayerList"]:
+#        for renderLayer in job["render_layer_list"]:
 #            res["renderLayerEnable"][renderLayer]= True
 
         f = file(self.get_file_path(), 'rb')
         for l in f:
             if version is None and 'version' in l:
                 version = l.split('version')[1][1:5]
-            if frameStart is None and ('fs\0 ' in l):
+            if frame_start is None and ('fs\0 ' in l):
                 # print "sisi"
                 print l.split('fs\0 ')[0]
                 print binascii.hexlify(l.split('fs\0 ')[0][-1])
-#                frameStart = struct.unpack('>i',l.split('fs\0 ')[0][1:5])[0]
-#                frameStart = l.split('fs')[1][1:5]
-                frameStart = struct.unpack('>i', l.split('fs\0 ')[0][-4:])[0]
-                print str(frameStart)
-            if frameEnd is None and ('ef\0 ' in l):
+#                frame_start = struct.unpack('>i',l.split('fs\0 ')[0][1:5])[0]
+#                frame_start = l.split('fs')[1][1:5]
+                frame_start = struct.unpack('>i', l.split('fs\0 ')[0][-4:])[0]
+                print str(frame_start)
+            if frame_end is None and ('ef\0 ' in l):
                 print binascii.hexlify(l.split('ef\0 ')[0][-1])
-                frameEnd = struct.unpack('>i', l.split('ef\0 ')[0][-4:])[0]
-                print str(frameEnd)
-            if byFrameSteps is None and ('bfs ' in l):
+                frame_end = struct.unpack('>i', l.split('ef\0 ')[0][-4:])[0]
+                print str(frame_end)
+            if by_frame_steps is None and ('bfs ' in l):
                 print binascii.hexlify(l.split('bfs ')[0][-1])
-                byFrameSteps = struct.unpack('>i', l.split('bfs ')[0][-4:])[0]
-                print str(byFrameSteps)
-            if fpsData is None and 'TUNI' in l:
+                by_frame_steps = struct.unpack('>i', l.split('bfs ')[0][-4:])[0]
+                print str(by_frame_steps)
+            if fps_data is None and 'TUNI' in l:
 
-                type = l.split('TUNI')[1][1:100]
-                fpsData = 0
+                movie_type = l.split('TUNI')[1][1:100]
+
                 print "test-"
-                print type
-                if 'ntscf' in type:
-                    fpsData = 60
-                    typeInfo = 'ntscf'
-                elif 'ntsc' in type:
-                    fpsData = 30
-                    typeInfo = 'ntsc'
-                elif 'palf' in type:
-                    fpsData = 50
-                    typeInfo = 'palf'
-                elif 'pal' in type:
-                    fpsData = 25
-                    typeInfo = 'pal'
-                elif 'film' in type:
-                    fpsData = 24
-                    typeInfo = 'film'
-                elif 'game' in type:
-                    fpsData = 15
-                    typeInfo = 'game'
-                elif 'show' in type:
-                    fpsData = 48
-                    typeInfo = 'show'
+                print movie_type
+                if 'ntscf' in movie_type:
+                    fps_data = 60
+                    type_info = 'ntscf'
+                elif 'ntsc' in movie_type:
+                    fps_data = 30
+                    type_info = 'ntsc'
+                elif 'palf' in movie_type:
+                    fps_data = 50
+                    type_info = 'palf'
+                elif 'pal' in movie_type:
+                    fps_data = 25
+                    type_info = 'pal'
+                elif 'film' in movie_type:
+                    fps_data = 24
+                    type_info = 'film'
+                elif 'game' in movie_type:
+                    fps_data = 15
+                    type_info = 'game'
+                elif 'show' in movie_type:
+                    fps_data = 48
+                    type_info = 'show'
                 else:
                     print "else"
-                    fpsData = 60
+                    fps_data = 60
             if renderer is None and 'ren\0' in l:
                 ren = l.split('ren\0')[1][1:100]
                 if 'mentalRay' in ren:
@@ -96,73 +98,77 @@ class FileParserMayaMB(filelib.parser.ma.FileParserMaya):
                     print ren
                     renderer = 'maya_software'
             if "RNDLCREA" in l:
-                renderLayerInfoList = l.split("RNDLCREA")
-                for renderLayerInfo in renderLayerInfoList:
-                    layerInfo = renderLayerInfo[0:50]
-                    layerNameInfoList = layerInfo.split('\0')
-                    for layerNameInfo in layerNameInfoList:
-                        if layerNameInfo != '':
-                            if layerNameInfo == 'masterLayer':
-                                layerNameInfo = 'defaultRenderLayer'
-                            if len(layerNameInfo) > 1 and layerNameInfo.isalnum() and 'DBLE' not in layerNameInfo and 'FLGS' not in layerNameInfo and 'rlmi' not in layerNameInfo and 'rndr' not in layerNameInfo:
-                                renderLayerList.append(layerNameInfo)
-#                for renderLayerInfo in renderLayerInfoList:
-#                    layerInfo = renderLayerInfo[0:100];
-#                    if 'rndr' in layerInfo:
-#                       for renderLayer in renderLayerList:
-#                           if renderLayer == '':
-#                               continue
-#                           rendLayer = renderLayer
-#                           if renderLayer == 'masterLayer':
-#                               rendLayer = 'defaultRenderLayer'
-#                           if rendLayer in layerInfo:
-#                               renderLayerEnable[renderLayer] = False
+                render_layer_info_list = l.split("RNDLCREA")
+                for renderLayerInfo in render_layer_info_list:
+                    layer_info = renderLayerInfo[0:50]
+                    layer_name_info_list = layer_info.split('\0')
+                    for layer_name_info in layer_name_info_list:
+                        if layer_name_info != '':
+                            if layer_name_info == 'masterLayer':
+                                layer_name_info = 'defaultRenderLayer'
+                            if len(layer_name_info) > 1 and layer_name_info.isalnum() and 'DBLE' not in layer_name_info:
+                                if 'FLGS' not in layer_name_info:
+                                    if 'rlmi' not in layer_name_info:
+                                        if 'rndr' not in layer_name_info:
+                                            render_layer_list.append(layer_name_info)
+            #                for renderLayerInfo in render_layer_info_list:
+            #                    layer_info = renderLayerInfo[0:100];
+            #                    if 'rndr' in layer_info:
+            #                       for renderLayer in render_layer_list:
+            #                           if renderLayer == '':
+            #                               continue
+            #                           rendLayer = renderLayer
+            #                           if renderLayer == 'masterLayer':
+            #                               rendLayer = 'defaultRenderLayer'
+            #                           if rendLayer in layer_info:
+            #                               renderLayerEnable[renderLayer] = False
             if "DCAMCREA" in l:
-                cameraInfoList = l.split("DCAMCREA")
-                for cameraInfo in cameraInfoList:
-                    camInfo = cameraInfo[0:50]
-                    cameraNameInfoList = camInfo.split('\0')
-                    size = len(cameraNameInfoList)
+                camera_info_list = l.split("DCAMCREA")
+                for camera_info in camera_info_list:
+                    cam_info = camera_info[0:50]
+                    camera_name_info_list = cam_info.split('\0')
+
                     i = 0
-                    for cameraNameInfo in cameraNameInfoList:
-                        if cameraNameInfo != '':
+                    for camera_name_info in camera_name_info_list:
+                        if camera_name_info != '':
                             if i >= 1:
-                                if len(cameraNameInfo) > 1 and 'DBL3' not in cameraNameInfo and 'XFRMCREA' not in cameraNameInfo:
-                                    cameraList.append(cameraNameInfo)
-#                                   print cameraNameInfo
+                                if len(camera_name_info) > 1 and 'DBL3' not in camera_name_info:
+                                    if 'XFRMCREA' not in camera_name_info:
+                                        camera_list.append(camera_name_info)
+    #                                   print camera_name_info
                                 break
-                            i+=1
-        if frameStart is None:
+                            i += 1
+        if frame_start is None:
             self._param["startFrame"] = "1"
         else:
             # print "tx"
-            # print frameStart
-            # print fpsData
-            self._param["startFrame"]=str(frameStart*fpsData/100.0/60.0)
-        if frameEnd is None:
+            # print frame_start
+            # print fps_data
+            self._param["startFrame"] = str(frame_start*fps_data/100.0/60.0)
+        if frame_end is None:
             self._param["endFrame"] = "10"
         else:
-            self._param["endFrame"]=str(frameEnd*fpsData/100.0/60.0)
-        if byFrameSteps is None:
-            byFrameSteps = "1"
+            self._param["endFrame"] = str(frame_end*fps_data/100.0/60.0)
+        if by_frame_steps is None:
+            by_frame_steps = "1"
         if renderer is None:
             renderer = 'sw'
 
         self._param["version"] = version
         self._param["application"] = "Maya_"+version
         self._param["renderer"] = renderer
-        self._param["type"] = typeInfo
+        self._param["movie_type"] = type_info
 
-        self._param["byFrameSteps"] = str(byFrameSteps)
+        self._param["by_frame_steps"] = str(by_frame_steps)
 
-        self._param["outputFileExt"] = outputFileExt
-        self._param["outputFileExtVray"] = outputFileExtVray
-        self._param["outputPathFormat"] = outputPathFormat
-        self._param["opuputPathFormatVray"] = outputPathFormatVray
+        self._param["output_file_ext"] = output_file_ext
+        self._param["output_file_ext_vray"] = output_file_ext_vray
+        self._param["output_path_format"] = output_path_format
+        self._param["opuputPathFormatVray"] = output_path_format_vray
 
-        self._param["renderLayerList"] = renderLayerList
-        self._param["renderLayerEnable"] = renderlayerEnable
-        self._param["cameraList"] = cameraList
+        self._param["render_layer_list"] = render_layer_list
+        self._param["renderLayerEnable"] = render_layer_enable
+        self._param["camera_list"] = camera_list
 
         print self._param
 
