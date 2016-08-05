e3371d2c2dba7aa228b1ee10e1233e960eae2529
Masataka Koike <plinecom@gmail.com>
2016-08-05 18:42:09 +0900

    Refactor

--- gui/submit/dispatcher/qube.py
+++ gui/submit/dispatcher/qube.py
@@ -42,8 +42,8 @@ class QubePanel(QtGui.QWidget):
 
         self.pool_label = QtGui.QLabel("cluster")
         self.pool_combo = QtGui.QComboBox(self)
-        print "ccc"
-        print self._jobList.get_current_job().getvalue("[*].dispatcherInfo.[0].pools")
+        # print "ccc"
+        # print self._jobList.get_current_job().getvalue("[*].dispatcherInfo.[0].pools")
         self.pool_combo.addItems(self._jobList.get_current_job().getvalue("[*].dispatcherInfo.[0].pools")[0])
         layout.addWidget(self.pool_label, height, 0)
         layout.addWidget(self.pool_combo, height, 1)
